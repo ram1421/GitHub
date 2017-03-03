d126275bb6b60277d688c6c4fb4b8246d851e0e5
Bhargava Donthini <Bhargava.Donthini@coxautoinc.com>
2017-03-03 16:12:12 -0500

    Added Git.Test

---
+++ GitHub.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHub.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GitTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
